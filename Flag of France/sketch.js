3a3f9dc934f30c1361a0d480a0c197b0896b0f5d
G7-WHJR <raveen.anand@whitehatjr.com>
2021-06-26 14:27:11 +0530

    V1

---
+++ Flag of France/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(450, 400);
+
+  //create sprite for flag stripes using function by passing parameters of x position and color
+
+  //Stripe 1 : x-position : 140, color : "blue"
+
+  //Stripe 1 : x-position : 215, color : "white"
+
+  //Stripe 1 : x-position : 290, color : "red"
+}
+
+function draw() {
+  background("honeydew");
+  drawSprites();
+}
